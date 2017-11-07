ad7d8b724405eb66a57a07975cb85f10509734de
Albert Septiawan <albert.septiawan@ansvia.com>
2017-11-07 11:16:31 +0700

    add(user disconnect notification), update chat color for user chat, user disconnect, user connect(join)

--- index.js
+++ index.js
@@ -11,19 +11,23 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket){
-  // console.log('a user connected');
+  var g_username;
+
+  console.log('a user trying to connect ...');
   // io.emit('user-conn', 'a user connected');
   // socket.broadcast.emit('user-conn', 'a user connected');
-  //
-  // socket.on('disconnect', function() {
-  //   // console.log('a user disconnected');
-  //   socket.broadcast.emit('user-disconn', 'a user disconnected');
-  // });
 
   socket.on('user-join', function(username) {
+    g_username = username;
     const msg = username + ' telah bergabung';
     socket.broadcast.emit('user-join', msg);
     console.log(msg);
+  });
+
+  socket.on('disconnect', function() {
+    const msg = g_username + ' telah pergi ...';
+    console.log(msg);
+    socket.broadcast.emit('user-disconn', msg);
   });
 
   socket.on('chat-msg', function(msgperson) {
